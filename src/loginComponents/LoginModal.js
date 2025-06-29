b9f394cba39dac63cdc4a081057de249d5acb0df
Amanda Nwadukwe <amandanwadukwe@gmail.com>
2025-06-29 14:51:57 +0100

    fixing payment processing error bug

--- src/loginComponents/LoginModal.js
+++ src/loginComponents/LoginModal.js
@@ -12,7 +12,7 @@ const handleSubmit = async (e) => {
     console.log("Submitting with:", { email, password }); // Add this debug line
     
     try {
-      const response = await fetch('https://amandanwadukwe.a2hosted.com/loud-mouth-finance/users/login', {
+      const response = await fetch('https://amandanwadukwe.a2hosted.com/loud-mouth-finance/api/users/login', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
